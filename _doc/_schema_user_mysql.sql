54b80aee3233ac0c5381fc382260275299f7bb63
deven <devenfan@gmail.com>
2018-05-20 15:06:38 +0800

    整理db

---
+++ _doc/_schema_user_mysql.sql
@@ -0,0 +1,82 @@
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_authority`;
+CREATE TABLE `sys_authority`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_by` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_date` tinyblob NOT NULL,
+  `last_modified_by` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `last_modified_date` tinyblob,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_by` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_date` tinyblob NOT NULL,
+  `last_modified_by` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `last_modified_date` tinyblob,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_role_authorities
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_authorities`;
+CREATE TABLE `sys_role_authorities`  (
+  `sys_role_id` bigint(20) NOT NULL,
+  `authorities_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`sys_role_id`, `authorities_id`) USING BTREE,
+  INDEX `FK9oc88v6ini6fjy2spcb8d63no`(`authorities_id`) USING BTREE,
+  CONSTRAINT `FK9oc88v6ini6fjy2spcb8d63no` FOREIGN KEY (`authorities_id`) REFERENCES `sys_authority` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FKscivyriuji1w3sgrcpeqo5jdd` FOREIGN KEY (`sys_role_id`) REFERENCES `sys_role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_by` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_date` tinyblob NOT NULL,
+  `last_modified_by` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `last_modified_date` tinyblob,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `first_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `image_url` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `last_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `password` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `UK_51bvuyvihefoh4kp5syh2jpi4`(`username`) USING BTREE,
+  UNIQUE INDEX `UK_ahtq5ew3v0kt1n7hf1sgp7p8l`(`email`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user_roles
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_roles`;
+CREATE TABLE `sys_user_roles`  (
+  `sys_user_id` bigint(20) NOT NULL,
+  `roles_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`sys_user_id`, `roles_id`) USING BTREE,
+  INDEX `FKdpvc6d7xqpqr43dfuk1s27cqh`(`roles_id`) USING BTREE,
+  CONSTRAINT `FKd0ut7sloes191bygyf7a3pk52` FOREIGN KEY (`sys_user_id`) REFERENCES `sys_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FKdpvc6d7xqpqr43dfuk1s27cqh` FOREIGN KEY (`roles_id`) REFERENCES `sys_role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ _doc/_schema_user_pgsql.sql
@@ -0,0 +1,128 @@
+
+-- ----------------------------
+-- Sequence structure for hibernate_sequence
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."hibernate_sequence";
+CREATE SEQUENCE "public"."hibernate_sequence"
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Table structure for sys_authority
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."sys_authority";
+CREATE TABLE "public"."sys_authority" (
+  "id" int8 NOT NULL DEFAULT NULL,
+  "created_by" varchar(50) COLLATE "pg_catalog"."default" NOT NULL DEFAULT NULL,
+  "created_date" bytea NOT NULL DEFAULT NULL,
+  "last_modified_by" varchar(50) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "last_modified_date" bytea DEFAULT NULL,
+  "name" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "value" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL
+)
+;
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."sys_role";
+CREATE TABLE "public"."sys_role" (
+  "id" int8 NOT NULL DEFAULT NULL,
+  "created_by" varchar(50) COLLATE "pg_catalog"."default" NOT NULL DEFAULT NULL,
+  "created_date" bytea NOT NULL DEFAULT NULL,
+  "last_modified_by" varchar(50) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "last_modified_date" bytea DEFAULT NULL,
+  "name" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "value" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL
+)
+;
+
+-- ----------------------------
+-- Table structure for sys_role_authorities
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."sys_role_authorities";
+CREATE TABLE "public"."sys_role_authorities" (
+  "sys_role_id" int8 NOT NULL DEFAULT NULL,
+  "authorities_id" int8 NOT NULL DEFAULT NULL
+)
+;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."sys_user";
+CREATE TABLE "public"."sys_user" (
+  "id" int8 NOT NULL DEFAULT NULL,
+  "created_by" varchar(50) COLLATE "pg_catalog"."default" NOT NULL DEFAULT NULL,
+  "created_date" bytea NOT NULL DEFAULT NULL,
+  "last_modified_by" varchar(50) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "last_modified_date" bytea DEFAULT NULL,
+  "email" varchar(100) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "first_name" varchar(50) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "image_url" varchar(256) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "last_name" varchar(50) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "password" varchar(60) COLLATE "pg_catalog"."default" NOT NULL DEFAULT NULL,
+  "username" varchar(50) COLLATE "pg_catalog"."default" NOT NULL DEFAULT NULL
+)
+;
+
+-- ----------------------------
+-- Table structure for sys_user_roles
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."sys_user_roles";
+CREATE TABLE "public"."sys_user_roles" (
+  "sys_user_id" int8 NOT NULL DEFAULT NULL,
+  "roles_id" int8 NOT NULL DEFAULT NULL
+)
+;
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+SELECT setval('"public"."hibernate_sequence"', 2, false);
+
+-- ----------------------------
+-- Primary Key structure for table sys_authority
+-- ----------------------------
+ALTER TABLE "public"."sys_authority" ADD CONSTRAINT "sys_authority_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table sys_role
+-- ----------------------------
+ALTER TABLE "public"."sys_role" ADD CONSTRAINT "sys_role_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table sys_role_authorities
+-- ----------------------------
+ALTER TABLE "public"."sys_role_authorities" ADD CONSTRAINT "sys_role_authorities_pkey" PRIMARY KEY ("sys_role_id", "authorities_id");
+
+-- ----------------------------
+-- Uniques structure for table sys_user
+-- ----------------------------
+ALTER TABLE "public"."sys_user" ADD CONSTRAINT "uk_ahtq5ew3v0kt1n7hf1sgp7p8l" UNIQUE ("email");
+ALTER TABLE "public"."sys_user" ADD CONSTRAINT "uk_51bvuyvihefoh4kp5syh2jpi4" UNIQUE ("username");
+
+-- ----------------------------
+-- Primary Key structure for table sys_user
+-- ----------------------------
+ALTER TABLE "public"."sys_user" ADD CONSTRAINT "sys_user_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table sys_user_roles
+-- ----------------------------
+ALTER TABLE "public"."sys_user_roles" ADD CONSTRAINT "sys_user_roles_pkey" PRIMARY KEY ("sys_user_id", "roles_id");
+
+-- ----------------------------
+-- Foreign Keys structure for table sys_role_authorities
+-- ----------------------------
+ALTER TABLE "public"."sys_role_authorities" ADD CONSTRAINT "fk9oc88v6ini6fjy2spcb8d63no" FOREIGN KEY ("authorities_id") REFERENCES "sys_authority" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."sys_role_authorities" ADD CONSTRAINT "fkscivyriuji1w3sgrcpeqo5jdd" FOREIGN KEY ("sys_role_id") REFERENCES "sys_role" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Keys structure for table sys_user_roles
+-- ----------------------------
+ALTER TABLE "public"."sys_user_roles" ADD CONSTRAINT "fkd0ut7sloes191bygyf7a3pk52" FOREIGN KEY ("sys_user_id") REFERENCES "sys_user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."sys_user_roles" ADD CONSTRAINT "fkdpvc6d7xqpqr43dfuk1s27cqh" FOREIGN KEY ("roles_id") REFERENCES "sys_role" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
